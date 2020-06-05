fb86ad03eeab4193c5dfab9976abe0f5653da293
Sarah Rose <sarahrose63089@gmail.com>
2020-06-05 14:47:03 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,6 +8,8 @@ def find_min_in_nested_arrays(src)
     inner_count = 0
     while inner_count < src[count][inner_count] do
       if src[count][inner_count] < smallest
+        puts smallest
+        binding.pry
         smallest = src[count][inner_count]
       end
       inner_count += 1
